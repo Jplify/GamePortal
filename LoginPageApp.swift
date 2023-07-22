3530aacbc9b2008b8c3e9f8360f56c1804982514
Juan Posada <59146925+Jplify@users.noreply.github.com>
2023-07-22 19:36:09 -0400

    add files via upload
    login page interface

---
+++ LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by user242205 on 7/22/23.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginViewer()
+        }
+    }
+}
